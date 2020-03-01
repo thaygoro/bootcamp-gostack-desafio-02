7d08e9f9db87be3825ff4d886be159837f298cca
Tháygoro <thaygoro@gmail.com>
2020-02-29 22:01:49 -0300

    Sequelize configured

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'ffdb123',
+  database: 'fastfeetdb',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+};
